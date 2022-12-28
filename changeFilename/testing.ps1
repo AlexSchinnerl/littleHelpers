d86148adc0fe4903bfccbca8d86fad6f30d582c9
Alex Schinnerl <alex.itsme@gmx.at>
2022-12-28 14:52:51 +0100

    delete trailing ;

---
+++ changeFilename/testing.ps1
@@ -0,0 +1,8 @@
+# $prefix = Read-Host "enter prefix"
+
+$output = "K12102728@students.jku.at; K12103993@students.jku.at; K12116248@students.jku.at; K12102915@students.jku.at; K12103850@students.jku.at; K12102695@students.jku.at; "
+$output.Length
+
+$output = $output.Substring(0,$output.Length-2)
+$output
+$output.Length
--- studentMail/mailGenerator.ps1
+++ studentMail/mailGenerator.ps1
@@ -1,8 +1,10 @@
 Clear-Content .\output.txt #clear output File
 $data = Get-Content -Path .\input.txt # read content from input File
 $data = $data -replace "[^0-9]" # delete each char. that is no number
-# $data = $data -replace "\n","" # clear breaks - ?
-# Write-Output $data
-$mail = $data | ForEach-Object{"K$_@students.jku.at; "} # create mail adress
-# Write-Output $mail
+$mail = $data | ForEach-Object{"K"+$_+"@students.jku.at; "} # create mail adress
 $mail | Out-File -FilePath .\output.txt -Append -NoNewline # write mail adress in output file NB! NoNewLine
+$data = Get-Content -Path .\output.txt
+$mail = $data.Substring(0,$data.Length-2) # delete last 2 characters ("; ")
+Write-Output $mail
+Clear-Content .\output.txt #clear output File
+$mail | Out-File -FilePath .\output.txt -Append -NoNewline # write mail adress in output file
