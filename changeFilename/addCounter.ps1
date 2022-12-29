1fcf73361bdcec9e5c8a4161e10e6cf0c4cc1d45
Alex Schinnerl <alex.itsme@gmx.at>
2022-12-29 10:05:24 +0100

    add suffix and add counter

---
+++ changeFilename/addCounter.ps1
@@ -0,0 +1,8 @@
+﻿# delete something from filename
+# Get-ChildItem -Path "." | Rename-Item -NewName {$_.Name -replace "193137-", ""}
+
+
+# $suffix = Read-Host "enter suffix"
+$count = 1
+$items = Get-ChildItem "*.txt" -Path "./files2change" 
+$items | Rename-Item -NewName {$_.BaseName + $Script:count++ + $_.Extension} # adds counter between filename and extention
---
+++ changeFilename/addSuffix.ps1
@@ -0,0 +1,8 @@
+﻿# delete something from filename
+# Get-ChildItem -Path "." | Rename-Item -NewName {$_.Name -replace "193137-", ""}
+
+
+$suffix = Read-Host "enter suffix"
+
+$items = Get-ChildItem "*.txt" -Path "./files2change" 
+$items | Rename-Item -NewName {$suffix + "_" + $_.Name}
